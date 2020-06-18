fbc59553fc6265de3834e072091957882490efc6
Connor Foster <foster.clee@gmail.com>
2020-06-18 17:40:32 -0500

    Fetch from Server

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -89,7 +90,7 @@ function RenderCampsite({ campsite }) {
     return (
         <div className="col-md-5 m-1">
             <Card>
-                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
                 <CardBody>
                     <CardText>{campsite.description}</CardText>
                 </CardBody>
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 import { Loading } from './LoadingComponent';
-
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderCard({ item, isLoading, errMess }) {
     if (isLoading) {
@@ -16,7 +16,7 @@ function RenderCard({ item, isLoading, errMess }) {
     }
     return (
         <Card>
-            <CardImg src={item.image} alt={item.name} />
+            <CardImg src={baseUrl + item.image} alt={item.name} />
             <CardBody>
                 <CardTitle>{item.name}</CardTitle>
                 <CardText>{item.description}</CardText>
@@ -26,18 +26,23 @@ function RenderCard({ item, isLoading, errMess }) {
 }
 
 function Home(props) {
+    console.log(props);
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md m-1">
                     <RenderCard
                         item={props.campsite}
-                        isLoading={props.campsitesLoading}
-                        errMess={props.campsitesErrMess}
+                        isLoading={props.campsiteLoading}
+                        errMess={props.campsiteErrMess}
                     />
                 </div>
                 <div className="col-md m-1">
-                    <RenderCard item={props.promotion} />
+                    <RenderCard
+                        item={props.promotion}
+                        isLoading={props.promotionLoading}
+                        errMess={props.promotionErrMess}
+                    />
                 </div>
                 <div className="col-md m-1">
                     <RenderCard item={props.partner} />

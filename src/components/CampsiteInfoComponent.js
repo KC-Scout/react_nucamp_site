2e4e4714aacf9a6e695361cb51ec3abf1b63b76a
Connor Foster <foster.clee@gmail.com>
2020-06-20 10:59:06 -0500

    Item Undefined Error Message

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardimgOverlay, CardText, CardBody, CardTitle, CardImgOverlay, Breadcrumb, BreadcrumbItem, Button, Modal, Row, Label, ModalHeader, ModalBody, Form, FormGroup, Input } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, Label, ModalHeader, ModalBody } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { render } from '@testing-library/react';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
